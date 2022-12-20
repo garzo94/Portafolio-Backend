25a4f1018bd850565165abd5d658766f196b1d46
garzo94 <alexgarzo25@gmail.com>
2022-12-20 13:29:01 -0600

    s3

--- portafolio/settings.py
+++ portafolio/settings.py
@@ -166,17 +166,20 @@ STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIR = [
     os.path.join(BASE_DIR,'static')
 ]
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
 
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
 
 AWS_ACCESS_KEY_ID = 'AKIAT2DPK7XMU75V6I3S'
 AWS_SECRET_ACCESS_KEY =  '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'
 AWS_STORAGE_BUCKET_NAME = 'myportfolio-garzo94'
 AWS_QUERYSTRING_AUTH = False
 AWS_S3_FILE_OVERWRITE = False
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
